f87fafe0aa33569b202b0e86f482aac187f56f0f
jarodriguez69 <rodriguezjose519@gmail.com>
2025-02-18 20:46:55 -0300

    a

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Article;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        
+
+       
+
+        User::factory(10)->create();
+        Article::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+             'name' => 'José Rodríguez',
+             'email' => 'rodriguezjose519@gmail.com',
+             'password' => Hash::make('123')]);
     }
 }
